e95df250929682bb2a6c5a8959efd0f7aa2168e4
bharatgavade <bharatgavade07@gmail.com>
2022-03-12 19:23:49 +0530

    String

---
+++ JavaScript/string.js
@@ -0,0 +1,26 @@
+// combination of alpha & speicalchars
+// special chars -@#$%^)()
+
+let password = "QweT&@#3691"
+console.log(password)
+console.log(typeof password)
+
+// 1. How to make all characters in lowercase
+console.log(password.toLowerCase())
+
+// 2. How to make all characters in uppercase
+console.log(password.toUpperCase())
+
+//3. how to I check if string contains some characters.
+console.log(password.includes("692"))
+
+// 4. How to find index of a character in string.
+console.log(password.indexOf("#"))
+
+//5. How to i check if string ends with some characters.
+console.log(password.endsWith("91"))
+
+// 6. print from charater how many string is there
+let empname="dadvdid"
+console.log(empname.indexOf("d",2))
+
