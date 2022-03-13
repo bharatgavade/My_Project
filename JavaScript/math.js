8651863e69a5a9fa8f57e98f9f356d6477fdde86
bharatgavade <bharatgavade07@gmail.com>
2022-03-13 19:21:14 +0530

    Math object

---
+++ JavaScript/math.js
@@ -0,0 +1,29 @@
+// Math object in JS
+
+// How to round a decimal value. (4.6)
+
+console.log(Math.round(4.6))
+
+// Math.round returns nearest integer
+
+console.log(Math.round(4.3)) //4
+console.log(Math.round(4.5))  //5
+
+// how to get nearest intest round up
+
+console.log(Math.ceil(4.3) ) //5
+
+// how to get nearest intest round down
+
+console.log(Math.floor(4.9) ) //4
+
+// Get Squroor of a number. 
+console.log(Math.sqrt(49)); //7
+
+// Max value from a colleciton of values.
+console.log(Math.max(9, 10 , 8, 45, 78, 32, 21)); // 78
+
+// Min value from a colleciton of values.
+console.log(Math.min(9, 10 , 8, 45, 78, 32, 21)); // 8
+
+
